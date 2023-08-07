1e28c118d04cb9d44b9ec8794f1e64ca29dc7d89
Moses Oyewole <mmosix@gmail.com>
2023-08-07 04:14:42 +0000

    uhn

--- router/communities.js
+++ router/communities.js
@@ -105,44 +105,77 @@ router.get('/my', verifyToken, (req, res) =>{
 
   
 // Retrieve recomended community for user 
-router.get('/recommend', verifyToken, (req, res) =>{
+// router.get('/recommend', verifyToken, (req, res) =>{
+
+//     //verify the JWT token generated for the community
+//     jsonwebtoken.verify(req.token, privateKey, async(err, authorizedData) => {
+//         if(err){
+//             //If error send Forbidden (403)
+//             res.sendStatus(403);
+//         } else {
+//             //If token is successfully verified, we can send the autorized data 
+
+//     const user_id = authorizedData.id;
+//     const user = await conn.getUserByID(user_id);
+//     const mood = user.mood;
+  
+//     if (!mood) {
+//         return res.status(400).send({ error: true, message: 'Please provide mood'+ mood });
+//     }
+  
+//     db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id WHERE C.moods LIKE '%?%' GROUP BY C.id", user_id, mood, function (error, results, fields) {
+//         if (error) throw error;
+        
+//         return res.send({ error: false, data: results, message: 'Recommended community list.' });
+
+//           // If the first element does not exist
+//         //   if (!results.length) {
+//         //     db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id GROUP BY C.id", mood, function (err, results2, fields) {
+//         //       if (err) throw err;
+//         //       return res.send({ error: false, data: results2, message: 'Recommended community list.' });
+//         //     });
+//         //   } else {
+//         //     return res.send({ error: false, data: results, message: 'Recommended community list.' });
+//         //   }
+
+
+//     });
+
+//         }
+//     })
+// });
+
+
+router.get('/recommend', verifyToken, (req, res) => {
 
     //verify the JWT token generated for the community
     jsonwebtoken.verify(req.token, privateKey, async(err, authorizedData) => {
         if(err){
-            //If error send Forbidden (403)
-            res.sendStatus(403);
+            //If error send 
+        res.status(422).send({ error: true,  message: 'Please provide authorization token' });
         } else {
             //If token is successfully verified, we can send the autorized data 
 
-    const user_id = authorizedData.id;
-    const user = await conn.getUserByID(user_id);
-    const mood = user.mood;
+            
+    let user_id = authorizedData.id;
+    let user = await conn.getUserByID(user_id);
+    let mood = user.mood;
   
     if (!mood) {
         return res.status(400).send({ error: true, message: 'Please provide mood'+ mood });
     }
-  
-    db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id WHERE C.moods LIKE '%?%' GROUP BY C.id", user_id, mood, function (error, results, fields) {
+            
+    db.query('SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id WHERE C.moods LIKE "%?%" GROUP BY C.id', user_id, mood,  function (error, results, fields) {
         if (error) throw error;
-        
-        return res.send({ error: false, data: results, message: 'Recommended community list.' });
 
-          // If the first element does not exist
-        //   if (!results.length) {
-        //     db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id GROUP BY C.id", mood, function (err, results2, fields) {
-        //       if (err) throw err;
-        //       return res.send({ error: false, data: results2, message: 'Recommended community list.' });
-        //     });
-        //   } else {
-        //     return res.send({ error: false, data: results, message: 'Recommended community list.' });
-        //   }
-
-
+        return res.send({ 
+            error: false, 
+            data: results, 
+            message: 'community list.' });
     });
-
         }
     })
+
 });
 
  
