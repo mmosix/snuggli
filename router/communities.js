c9e75710ce0f7d9ab22d8b932ba70fb8fef2207b
Moses Oyewole <mmosix@gmail.com>
2023-08-07 02:33:26 +0000

    another fix

--- router/communities.js
+++ router/communities.js
@@ -127,10 +127,10 @@ router.get('/recommend', verifyToken, (req, res) =>{
         if (error) throw error;
         else {
           // If the first element does not exist
-          if (results[0] == undefined) {
-            db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id WHERE C.moods LIKE '%?%' GROUP BY C.id", mood, function (err, results, fields) {
+          if (!results.length) {
+            db.query("SELECT C.*, COUNT(CF.user_id) AS followers, MAX(CF.user_id = ?) as i_follow FROM community C LEFT JOIN follow_community CF ON CF.community_id = C.id GROUP BY C.id", mood, function (err, results2, fields) {
               if (err) throw err;
-              return res.send({ error: false, data: results, message: 'Recommended community list.' });
+              return res.send({ error: false, data: results2, message: 'Recommended community list.' });
             });
           } else {
             return res.send({ error: false, data: results, message: 'Recommended community list.' });
