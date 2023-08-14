d690c06d43aeb1d61bcf9ee8ff38d0503bb5b29d
Moses Oyewole <mmosix@gmail.com>
2023-08-14 03:16:22 +0000

    fix public post

--- router/post.js
+++ router/post.js
@@ -309,15 +309,19 @@ router.post('/comment', (req, res) => {
             
   const query = `
   SELECT p.id AS post_id, p.content AS post_content, p.image_url AS post_image,
-    COUNT(pl.id) AS num_likes,
-    c.id AS comment_id, c.content AS comment_content, COUNT(cl.id) AS num_comment_likes
-  FROM posts p
-  INNER JOIN post_likes pl ON p.id = pl.post_id
-  LEFT JOIN comments c ON p.id = c.post_id
-  LEFT JOIN comment_likes cl ON c.id = cl.comment_id
-  WHERE p.is_public = 1
-  GROUP BY p.id, c.id
-  ORDER BY p.id, c.id
+      COUNT(pl.id) AS num_likes,
+      c.id AS comment_id, c.content AS comment_content, COUNT(cl.id) AS num_comment_likes
+    FROM posts p
+    INNER JOIN post_likes pl ON p.id = pl.post_id
+    LEFT JOIN (
+      SELECT post_id, id, content
+      FROM comments
+      ORDER BY post_id, id
+    ) c ON p.id = c.post_id
+    LEFT JOIN comment_likes cl ON c.id = cl.comment_id
+    WHERE p.is_public = 1
+    GROUP BY p.id, c.id
+    ORDER BY p.id, c.id
     `;
     
   db.query(query, (err, result) => {
