d3a68697bc170b40952186910a0c7b20d6ddfd09
Moses Oyewole <mmosix@gmail.com>
2023-08-18 17:55:21 +0000

    group post

--- router/post.js
+++ router/post.js
@@ -132,7 +132,7 @@ router.post('/create-group', verifyToken, (req, res) => {
             console.log('groupId:', groupId);
             console.log('postId:', postId);
   
-            if (!isPublic && groupId) {
+            if (!isPublic) {
               const insertGroupPostQuery =
                 'INSERT INTO group_posts (group_id, post_id) VALUES (?, ?)';
   
@@ -171,7 +171,7 @@ router.post('/create-group', verifyToken, (req, res) => {
   
           const postId = result.insertId;
   
-          if (!isPublic && groupId) {
+          if (!isPublic) {
             const insertGroupPostQuery = 'INSERT INTO group_posts (group_id, post_id) VALUES (?, ?)';
   
             db.query(insertGroupPostQuery, [groupId, postId], (err) => {
