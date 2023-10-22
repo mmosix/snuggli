268361252c762c79a30e5e825627160775db2583
Moses Oyewole <mmosix@gmail.com>
2023-10-22 15:46:39 +0000

    conn added to rec

--- router/communities.js
+++ router/communities.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { db } = require('../database');
+const { db, conn } = require('../database');
 const jsonwebtoken = require('jsonwebtoken');
 
 require('dotenv').config();
